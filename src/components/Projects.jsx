3af2e29c19645b74309a1b91147bc0ddb7d47d5a
Mauricio Portillo <106699864+Pohe1999@users.noreply.github.com>
2023-12-13 17:03:49 -0600

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,71 +5,64 @@ const Projects = () => {
     {
         title: 'Personal Website',
         images: ['/samuel-1.png', '/samuel-2.png', '/samuel-3.png'],
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi iusto doloremque enim odit sint',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi iusto doloremque enim odit sint Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi iusto doloremque enim odit sint',
         link: 'https://www.samuelhernandez.si',
       },
       {
-        title: 'Project 2',
-        images: ['/samuel-2.png', '/samuel-3.png', '/samuel-1.png'],
+        title: 'BankApp',
+        images: ['/bank-1.png', '/bank-2.png', '/bank-3.png'],
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi iusto doloremque enim odit sint',
-        link: '/project2.html',
+        link: 'https://bankappbymau.netlify.app',
       },
       {
-        title: 'Project 3',
-        images: ['/samuel-2.png', '/samuel-3.png', '/samuel-1.png'],
+        title: 'MarketPlace',
+        images: ['/market-1.png', '/market-2.png', '/market-3.png'],
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi iusto doloremque enim odit sint',
-        link: '/project3.html',
+        link: 'https://marketplacemau.netlify.app/',
       },
       {
-        title: 'Project 4',
-        images: ['/samuel-2.png', '/samuel-1.png', '/samuel-3.png'],
+        title: 'Payment Calculator',
+        images: ['/calculator.png', '/calculator.png', '/calculator.png'],
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi iusto doloremque enim odit sint',
-        link: '/project4.html',
-      },
-      {
-        title: 'Project 5',
-        images: ['/samuel-2.png', '/samuel-3.png', '/samuel-1.png'],
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi iusto doloremque enim odit sint',
-        link: '/project5.html',
-      },
-  ];
+        link: 'https://paymentcalculator.netlify.app',
+      }
+  ]
 
-  const [currentImage, setCurrentImage] = useState(0);
+  const [currentImage, setCurrentImage] = useState(0)
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentImage((prevImage) => (prevImage + 1) % 3); // 3 imágenes por tarjeta
-    }, 3000); // Cambia la imagen cada 3 segundos (3000 milisegundos)
+      setCurrentImage((prevImage) => (prevImage + 1) % 3) // 3 imágenes por tarjeta
+    }, 4000) // Cambia la imagen cada 3 segundos (3000 milisegundos)
 
-    return () => clearInterval(interval);
-  }, []);
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <section name="projects" className="pt-20 lg:pt-[120px] py-20 sm:py-32 bg-blue-800 text-gray-200 sm:px-16 px-8">
       <h2 className='text-center text-4xl sm:text-6xl font-thin'>
         Projects
       </h2>
-      <div className="container pt-8 sm:w-[3000px] w-[320px]">
-        <div className="sm:flex">
+      <h4 className='font-extralight text-center py-5'>// Here are some of my projects, take a look!</h4>
+      <p className='font-extralight text-center'>Swipe to see more</p>
+      <div className="overflow-x-auto">
+        <div className="flex whitespace-nowrap py-8">
           {dataCards.map((card, index) => (
-            <div key={index} className="w-full md:w-1/2 xl:w-1/3 px-4">
-              <div className="bg-gray-200 rounded-2xl overflow-hidden mb-10">
+            <div key={index} className="w-full md:w-1/2 xl:w-1/3 px-4 inline-block">
+              <div className="bg-gray-200 rounded-2xl overflow-hidden mb-10 sm:w-[400px] w-[255px]">
                 <img
                   src={card.images[currentImage]}
                   alt="image"
                   className="w-full"
                 />
                 <div className="p-8 sm:p-9 md:p-7 xl:p-9 text-center border-t-4 border-slate-900">
-                  <h3 className="text-pink-600 font-normal text-2xl mb-4 block hover:text-primary">
+                  <h3 className="text-pink-600 font-normal sm:text-4xl text-2xl mb-4 block hover:text-primary sm:pb-10 pb-20">
                     {card.title}
                   </h3>
-                  <p className="font-light text-black text-lg leading-relaxed mb-7 text-justify">
-                    {card.description}
-                  </p>
                   <a
                     target='_blank'
                     href={card.link}
-                    className="inline-block py-2 px-7 border border-slate-900 rounded-full text-xl font-extralight text-black hover:border-primary hover:bg-primary transition"
+                    className="font-extralight text-gray-900 text-xl border-2 border-blue-700 px-4 py-2 rounded-3xl"
                   >
                     View Site
                   </a>
@@ -80,7 +73,7 @@ const Projects = () => {
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Projects;
+export default Projects
